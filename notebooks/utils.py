ea9ca85288ba4ef551eae177d3956c079baf3de3
Kathryn Panger <kpanger88@gmail.com>
2024-03-27 20:20:57 -0400

    s2 cleaned

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -22,36 +22,39 @@ def convert_utf8(original_file_path: str, new_file_path: str):
 
 # Remove special characters when dealing with words
 # transforms removed characters into spaces, removes . and -
-def remove_special_for_words(my_string: str | Number):
-    return re.sub(r"[^a-zA-Z0-9]+", ' ', str(my_string))
+def remove_special_for_words(entry: str | Number):
+    return re.sub(r"[^a-zA-Z0-9]+", ' ', str(entry))
 
 # Remove special characters when dealing with numbers
 # No spaces, does not remove - and .
-def remove_special_for_numbers(my_string: str| Number):
-    return re.sub(r"[^a-zA-Z0-9-.]+", '', str(my_string))
+def remove_special_for_numbers(entry: str | Number):
+    return re.sub(r"[^a-zA-Z0-9-.]+", '', str(entry))
 
 # Remove leading and trailing spaces
-def truncate(my_string: str| Number):
-    return str(my_string).strip()
+def truncate(entry: str | Number):
+    return str(entry).strip()
 
 # Replace spaces with underscores
-def snake_case(my_string: str| Number):
-    return str(my_string).replace(" ", "_")
+def snake_case(entry: str | Number):
+    return str(entry).replace(" ", "_")
 
 # Make all letters lowercase
-def lower_case(my_string: str| Number):
-    return str(my_string).lower()
+def lower_case(entry: str | Number):
+    return str(entry).lower()
 
-def remove_spaces(my_string: str| Number):
-    return str(my_string).replace(" ", "")
+def remove_spaces(entry: str | Number):
+    return str(entry).replace(" ", "")
 
 # Get a deterministic hash of a string, dependent only on the seed
 # (for creating unique id columns which will always be consistent when fed the same data and seed, even if generated at different times)
-def deterministic_uuid(data:str| Number):
-    random.seed(str(data))
+def deterministic_uuid(entry: str | Number):
+    random.seed(str(entry))
     id = uuid.UUID(bytes=bytes(random.getrandbits(8) for _ in range(16)), version=4)
     id = str(id).replace("-", "")
     return id
+
+def cast_to_datetime(entry: str | Number):
+    return pd.to_datetime(str(entry))
 
 ########################
 # DataFrame Functions #
