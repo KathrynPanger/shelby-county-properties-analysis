79910dc35caa64d1cd4a53ac0b985a4bee3f2589
Kathryn Panger <kpanger88@gmail.com>
2024-04-02 20:16:38 -0400

    gitignore updated, csvs go for upload

--- extraction/utils.py
+++ extraction/utils.py
@@ -3,7 +3,7 @@ from pandas._libs.internals import defaultdict
 from unidecode import unidecode
 from collections.abc import Callable
 import re
-from typing import Optional
+from typing import Optional, Any
 import uuid
 import random
 from numbers import Number
@@ -45,13 +45,6 @@ def lower_case(entry: str | Number):
 def remove_spaces(entry: str | Number):
     return str(entry).replace(" ", "")
 
-# Get a deterministic hash of a string, dependent only on the seed
-# (for creating unique id columns which will always be consistent when fed the same data and seed, even if generated at different times)
-def deterministic_uuid(entry: str | Number):
-    random.seed(str(entry))
-    id = uuid.UUID(bytes=bytes(random.getrandbits(8) for _ in range(16)), version=4)
-    id = str(id).replace("-", "")
-    return id
 
 def cast_to_datetime(entry: str | Number):
     return pd.to_datetime(str(entry))
@@ -92,12 +85,38 @@ def clean_entries(df: pd.DataFrame,
 def cast_data_types(df: pd.DataFrame, names_to_types: dict) -> pd.DataFrame:
     return df.astype(names_to_types)
 
+# Get a deterministic hash of a string, dependent only on the seed
+# (for creating unique id columns which will always be consistent when fed the same data and seed, even if generated at different times)
+def deterministic_uuid(entry: str | Number):
+    random.seed(str(entry))
+    id = uuid.UUID(bytes=bytes(random.getrandbits(8) for _ in range(16)), version=4)
+    id = str(id).replace("-", "")
+    return id
 
 # Create a unique index column by taking a deterministic hash of values in selected identifier columns
-def set_unique_index(df: pd.DataFrame, columns_to_hash: list[str], index_name="id",
+def set_unique_index(df: pd.DataFrame,
+                     columns_to_hash: list[str],
+                     index_name="id",
                      index_length_limit: Optional[int] = None):
     df[index_name] = list(
         map(lambda x: deterministic_uuid(''.join([str(col_value) for col_value in x]))[0:index_length_limit],
             df[columns_to_hash].values))
     df.set_index(index_name, inplace=True)
     return df
+
+# Define a function for replacing a cell with only "-" with None, as this should be a missing value
+def convert_only_dash_to_missing(entry: Any) -> None:
+    entry = str(entry)
+    if entry =="-":
+        return None
+    else:
+        return entry
+
+def pilot_terminated_to_indicator(entry: any):
+    entry = str(entry)
+    if entry == "NO":
+        return 0
+    elif entry == "YES":
+        return 1
+    else:
+        return None
